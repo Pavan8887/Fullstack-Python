6c8852a4aa18ba77925dc0df09c73190c9f32ce2
Your Name <you@example.com>
2026-07-07 10:00:36 +0530

    Updated project

---
+++ operators1.py
@@ -0,0 +1,27 @@
+'''pduct_price =5000
+delevary_charges =100
+
+total_price = pduct_price + delevary_charges
+print(total_price)'''
+
+
+'''a=10
+b=3
+
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b)'''
+
+'''followers = 100
+total =followers + 1 
+total =followers -1
+print(total) '''
+
+
+saved_password = "abcd"
+enterd_password ="123"
+
+print(saved_password == enterd_password)
