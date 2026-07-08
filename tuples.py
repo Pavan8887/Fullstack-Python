40694efa90a0c7c9aa83f353a59daa6107ff5cb5
Your Name <you@example.com>
2026-07-08 09:44:56 +0530

    .

--- tuples.py
+++ tuples.py
@@ -1,10 +1,25 @@
-students = ("a","b","c")#tuples are collection of values and written inside paranthesis
-print(students[1])
+'''students = ("a","b","c")#tuples are collection of values and written inside paranthesis
+print(students[1])'''
 
-numbers = (1,2,3,4,5,)#tuples are collection of values and written inside paranthesis
-print(numbers[-2]) # we can acess tuples by their index numbers 
+'''numbers = (1,2,3,4,5,)#tuples are collection of values and written inside paranthesis
+print(numbers[-2]) # we can acess tuples by their index numbers '''
 
 '''data = (1,2,3)
 data[0]=100
 print(data)'''
+
+#TUPPLE METHODS
+'''x = (1,1,1,1,2,3,4,4,5,5,5,6,7,8,9)
+print(x.count(5))
+
+x = (1,1,1,1,2,3,4,4,5,5,5,6,7,8,9)
+print(x.count(5))
+print(x.index(5))'''
+
+#EXAMPLE 
+
+x = ("apple","banana","cherry","banana")
+print(x.count("banana"))
+
+
 
