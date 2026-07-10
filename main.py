bfed8b873fa845e1317658497ce3eca22b70dc84
Pavan8887 <pavankumarankolekar32@gmail.com>
2026-07-10 12:22:11 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# import  calci
+
+# result = calci.add(10,30)
+# print(result)
+
+##########################IMPORT SPECIFIC
+
+# from calci import add
+
+# from calci import sub
+
+# print(add(10,21))
+# print(sub(10,22))
+
+
+############################making shor form of modules
+
+# import calci as  c
+
+# print(c.add(10,20))
+
+
+#######################MATHEMATICAL CALCULATIONS USE MATH
+
+# import math
+
+# print(math.sqrt(10))
+
+###################IMPORTING RANDOM
+
+# import random
+
+# result = random.randint(10,20)
+# print(result)
--- oops.py
+++ oops.py
@@ -12,7 +12,7 @@
 # s1=student()
 # s1.details()
 
-#CONSTRUCTOR
+# CONSTRUCTOR
 
 # class Student():
 #     def __init__(self,name,age):
@@ -24,6 +24,123 @@
 # print(s1.name, s1.age)
 # print(s2.name, s2.age)
 # print(s3.name, s3.age)
+
+
+# CHECK BALANCE
+
+# class bank:
+#     def __init__(self,balance):
+#         self.balance = balance
+
+#     def check_balance(self):
+#         print(self.balance)
+
+# account = bank(5000)
+# account.check_balance()
+
+
+# class user:
+#     def __init__(self,name):
+#         self.name = name
+
+#     def login(self):
+#         print(self.name,"logged in")
+
+# u1 =user("nibba")
+# u2 =user("nimbu")
+# u3 =user("sushmita")
+# u1.login()
+# u2.login()
+# u3.login()
+
+
+        
+
+# class user:
+#     print("hi ")
+
+# class child(user):
+#     print("barish agaya")
+
+# class child2(child):
+#     print("ghar jaao ")
+
+# class daddy:
+#     def home(self):
+#         print("Respexct +++")
+
+# class mommy:
+#     def mane(self):
+#         print("Aura +++")
+# s = mommy()
+# s.home()
+# s.mane()
+
+# class tata:
+#     def land(self):
+#         print("AJJA SATT HODA")
+
+# class maga(tata):
+#     def jai(self):
+#         print("hengadipa")
+# class mommaga(maga):
+#     def owner(self):
+#         print("arama")
+
+# obj = mommaga()
+# obj.land()
+# obj.jai()
+# obj.owner()
+
+
+#MULTIPLE 
+
+# class brother():
+#     def bro(self):
+#         print("hi bro")
+# class sister():
+#     def sis(self):
+#         print("hi sister")
+# class cousin(brother,sister):
+#     def cou(self):
+#         print("HI this is my cousin")
+
+# sc = cousin()
+# sc.bro()
+# sc.sis()
+# sc.cou()
+
+#HIRARCHIAL METHODS
+
+# class appa():
+#     def papa(self):
+#         print("CHOCKLATE BEKU")
+
+# class amma(appa):
+#     def mommy(self):
+#         print("HUDUGI BEKU")
+# class tamma(appa):
+#     def bro(self):
+#         print("TANGI BEKU")
+
+# at = tamma()
+# at.papa()
+# ta = amma()
+# ta.papa()
+
+#__STR__ METHOD
+# class students:
+#     def __init__(self,name):
+#         self.name = name
+
+#     def __str__(self):
+#         return self.name
+    
+# s = students("shubh")
+# print(s)
+
+
+        
 
 
 
