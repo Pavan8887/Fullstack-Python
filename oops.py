5a3160ab731b08209a64bacba70029bbcad47aac
Your Name <you@example.com>
2026-07-10 09:28:07 +0530

    Initial commit

---
+++ oops.py
@@ -0,0 +1,16 @@
+# class Student:
+#     name ="pavan"
+#     def study(self):
+#         print("Penga is studying")
+# s1=Student()
+# print(s1.name)
+# s1.study()
+
+# class student:
+#     def details(self):
+#         print("hengidiya")
+# s1=student()
+# s1.details()
+
+
+
